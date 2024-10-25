db9bf35c38a41321bf9e0d8690a00a382ca6e8b6
Rebecca Nolan <G00423921@atu.ie>
2024-10-25 10:54:11 +0000

    Task 5 complete

--- server.js
+++ server.js
@@ -8,21 +8,48 @@ app.get('/', (req, res) => { //req- request res - response
     res.send('Welcome to Data Representation & Querying');
 });
 
-// app.get('/whatever', (req, res) => { 
-//     res.send('Whatever i want');
-// });
-
+//name
 app.get('/hello/:name', (req, res) => {
     const name = req.params.name;
     res.send(`Hello ${name}`);
 });
 
+//url parameters :name/:surname
 app.get('/hello/:name/:surname', (req, res) => {
     const name = req.params.name;
     const surname = req.params.surname;
     res.send(`Hello ${name} ${surname}`);
 });
 
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(200).json({ myMovies:movies }); //return JSON response
+    //res.json({ movies }); //sending array back
+});
+
+//error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
